f7f8b74acdbb2cbd546cbe1febcba556ed04ab5c
saintbassanaga <saintbassanaga01@gmail.com>
2023-04-27 06:04:14 +0100

    Importing Babel and setting Parameters

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
+
 """
-First you will setup a basic Flask app in 0-app.py. Create a single / route
-and an index.html template that simply outputs
-“Welcome to Holberton” as page title <title> and “Hello world” as header <h1>
+A Basic flask application
 """
 
 from flask import Flask, render_template
