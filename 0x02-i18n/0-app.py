788282901acf010d91a959a0b3a442440b24343e
saintbassanaga <saintbassanaga01@gmail.com>
2023-04-27 06:22:17 +0100

    Importing Babel and setting Parameters

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index()->str:  # put application's code here
+def index() -> str:
+    """
+    Renders a basic html template
+    """
     return render_template("0-index.html")
 
 
