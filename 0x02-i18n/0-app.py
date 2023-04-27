9a7c5c4703da45a1038f82a9399d1ce439d91949
saintbassanaga <saintbassanaga01@gmail.com>
2023-04-27 06:12:40 +0100

    Importing Babel and setting Parameters

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
+@app.route('/', strict_slashes=False)
+def index():  # put application's code here
     return render_template("0-index.html")
 
 
